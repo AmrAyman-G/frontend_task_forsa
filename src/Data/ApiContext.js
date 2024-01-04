8065e1ac86c3574d76fe63f7377ac7d708055186
amr ayman <100245739+AmrAyman-G@users.noreply.github.com>
2024-01-04 06:11:35 +0200

    FetchData function update 3/1/2024 🚀

--- src/Data/ApiContext.js
+++ src/Data/ApiContext.js
@@ -1,38 +1,19 @@
-import React, { createContext, useContext, useReducer, useEffect } from "react";
-import {
-  SET_SECTORS,
-  SET_BRANDS,
-  SET_OFFERS,
-  SET_ADDITIONAL_LOANS,
-} from "./actionsTypes";
-import {
-  sectorsReducer,
-  brandsReducer,
-  offersReducer,
-  additionalLoansReducer,
-} from "./apiRedusers";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { fetchData } from "./apiCallFunction";
 
 const dataContext = createContext();
 
 export const ApiProvider = ({ children }) => {
-  const [sectors, sectorsDispatch] = useReducer(sectorsReducer, []);
-  const [brands, brandsDispatch] = useReducer(brandsReducer, []);
-  const [offers, offersDispatch] = useReducer(offersReducer, []);
-  const [additionalLoans, additionalLoansDispatch] = useReducer(
-    additionalLoansReducer,
-    []
-  );
+  const [sectors, setSectors] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [offers, setOffers] = useState([]);
+  const [additionalLoans, setAdditionalLoans] = useState([]);
 
   useEffect(() => {
-    fetchData("stores/mysectors/", SET_SECTORS, sectorsDispatch);
-    fetchData("stores/mystores/?sector=1", SET_BRANDS, brandsDispatch);
-    fetchData("stores/myoffers/", SET_OFFERS, offersDispatch);
-    fetchData(
-      "onetransaction/myservicetypes/",
-      SET_ADDITIONAL_LOANS,
-      additionalLoansDispatch
-    );
+    fetchData("stores/mysectors/", setSectors);
+    fetchData("stores/mystores/?sector=1", setBrands);
+    fetchData("stores/myoffers/", setOffers);
+    fetchData("onetransaction/myservicetypes/", setAdditionalLoans);
   }, []);
 
   return (
